6efebc42f5fb2e73ba9b155efe23e4c2bdbf4795
joshuaking06 <joshua.king0692@gmail.com>
2019-01-03 15:49:15 -0800

    made user grids

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 $(() => {
-  const $h1 = $('h1')
 
-  $h1.text('hehe')
+
+  const $shipGrid = $('.ship-grid')
+  const $fireGrid = $('.fire-grid')
+  const width = 10
+
+  for(let i = 0; i<width*width; i++) {
+    $shipGrid.append($('<div />'))
+    $fireGrid.append($('<div />'))
+  }
+
+  
+
+
+
+
+
+
 
 
 })
