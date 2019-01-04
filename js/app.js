72979e92ac42db29b372799fbb1189de7bb99ac2
joshuaking06 <joshua.king0692@gmail.com>
2019-01-03 19:58:14 -0800

    established borders of ship grid

--- js/app.js
+++ js/app.js
@@ -23,41 +23,46 @@ $(() => {
   ship1.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
   $fireSquares.eq(fireIndex).addClass('firing')
 
-  // figure out which way user wants to move ship
+  // figure out which way user wants to move ship, and if it's possible
   $(document).on('keydown', e =>{
     const ship = ship1
     switch(e.keyCode){
-      case 37: moveShip(ship, 'left')
+      case 37: if(ship[ship.length-1] % width > ship.length-1)moveShip(ship, 'left')
         break
-      case 38: moveShip(ship, 'up')
+      case 38: if(ship[0] - width >= 0) moveShip(ship, 'up')
         break
-      case 39: moveShip(ship, 'right')
+      case 39: if(ship[0] % width < width-[ship.length]) moveShip(ship, 'right')
         break
-      case 40: moveShip(ship, 'down')
+      case 40: if(ship[0]+ width < width*width) moveShip(ship, 'down')
         break
     }
   })
 
+  // move the ship
   function moveShip(ship, direction){
-    ship.forEach(shipIndex => $shipSquares.eq(shipIndex).removeClass('ship'))
-    const nextIndex = getNextIndex(ship, direction)
+    const nextIndex = getNextIndex(ship, direction).sort((a,b) => b-a)
     console.log(nextIndex)
+    // console.log(nextIndex)
     nextIndex.forEach(index => {
+      ship.forEach(shipIndex => $shipSquares.eq(shipIndex).removeClass('ship'))
       ship.unshift(index)
       ship.pop()
+      ship.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
     })
-    ship.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
-
+    // console.log(ship)
+    return ship
   }
 
+  // determine where ship is going next
   function getNextIndex(ship, direction){
-    switch(direction) {
+    switch(direction){
       case 'right': return ship.map(index => index + 1)
       case 'left': return ship.map(index => index -1)
-      case 'down': return ship.map(index => index += width)
-      case 'up': return ship.map(index => index -= width)
+      case 'up': return ship.map(index => index - width)
+      case 'down': return ship.map(index => index + width)
     }
   }
+
 
 
 
