4c891a7e097cba0c228d5e471ed653257c6b3180
joshuaking06 <joshua.king0692@gmail.com>
2019-01-05 16:34:21 +0000

    set up multiple ship placement

--- js/app.js
+++ js/app.js
@@ -5,9 +5,13 @@ $(() => {
   const $shipGrid = $('.ship-grid')
   const $fireGrid = $('.fire-grid')
   const width = 10
-  const cpuGrid = []
-
-  const ship1 = [3,2,1,0]
+  const ships = {
+    carrier: [4,3,2,1,0],
+    battleship: [3,2,1,0],
+    destroyer: [2,1,0],
+    submarine: [2,1,0],
+    patrolBoat: [1,0]
+  }
   let fireIndex = 0
 
 
@@ -15,7 +19,7 @@ $(() => {
   for(let i = 0; i<width*width; i++) {
     $shipGrid.append($('<div />'))
     $fireGrid.append($('<div />'))
-    cpuGrid.push(i)
+    // cpuGrid.push(i)
   }
 
 
@@ -23,17 +27,26 @@ $(() => {
   // define the squares in each grid
   const $fireSquares = $fireGrid.find('div')
   const $shipSquares = $shipGrid.find('div')
-  // establish the ships/targeting reticule at start
-  ship1.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
-  $fireSquares.eq(fireIndex).addClass('firing')
 
-  placeShip(1)
+  function makeShip(size){
+    const randomRange = (Math.floor(Math.random() * 9) + 0)
+    const randomI = (randomRange *10)
+    return Array.from()
+  }
 
-  function placeShip(i){
+  // place the cpu ships on "board"
+  function placeCpuShips(){
+    let cpuShips = []
+    const cpuBattleship = makeShip(4)
+  }
 
+  function placeUserShips(i){
+    $(document).off()
+    // choosing ship to begin placing
+    const ship = Object.values(ships)[i-1]
+    ship.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
     // figure out which way user wants to move ship, and if it's possible
     $(document).on('keydown', e =>{
-      const ship = ship1
       switch(e.keyCode){
         case 37: if(ship[ship.length-1] % width > ship.length-1)moveShip(ship, 'left')
           break
@@ -43,24 +56,24 @@ $(() => {
           break
         case 40: if(ship[0]+ width < width*width) moveShip(ship, 'down')
           break
-        case 13: if(i < 1){
-          placeShip(i)
+        case 13: if(i-1 > 0){
+          i--
+          placeUserShips(i)
         } else aim()
       }
     })
   }
+
+
   // move the ship
   function moveShip(ship, direction){
     const nextIndex = getNextIndex(ship, direction).sort((a,b) => b-a)
-    console.log(nextIndex)
-    // console.log(nextIndex)
     nextIndex.forEach(index => {
       ship.forEach(shipIndex => $shipSquares.eq(shipIndex).removeClass('ship'))
       ship.unshift(index)
       ship.pop()
       ship.forEach(shipIndex => $shipSquares.eq(shipIndex).addClass('ship'))
     })
-    // console.log(ship)
     return ship
   }
 
@@ -87,6 +100,7 @@ $(() => {
 
   // aiming for enemy ship
   function aim(){
+    $fireSquares.eq(fireIndex).addClass('firing')
     $(document).off()
     $(document).on('keydown', e => {
       // left 37, up 38, right 39, down 40
@@ -107,9 +121,13 @@ $(() => {
 
 
 
+  // starting the game
+  function play(){
+    // placeCpuShips()
+    placeUserShips(5)
+  }
 
-
-
+  play()
 
 
 
